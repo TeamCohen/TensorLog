2e097d4d8304a2c49837dfdb2852334ec3b36b85
William Cohen <wcohen@cs.cmu.edu>
2016-05-19 09:57:05 -0400

    clean up

--- datasets/cora/cora-expt.py
+++ datasets/cora/cora-expt.py
@@ -1,5 +1,3 @@
-# current failure mode: inconsistent shapes computing Y-P
-
 import os.path
 import scipy.sparse as SS
 import scipy.io
@@ -20,35 +18,15 @@ def uncacheDB(dbFile):
         return matrixdb.MatrixDB.deserialize(dbFile)
 
 def uncacheMatPairs(cacheFile,dbFile,exampleFile):
-#    if not os.path.exists(cacheFile):
         db = uncacheDB(dbFile)
-#        print 'creating matrix pairs from %s...' % exampleFile
         print 'preparing examples...'
         d = expt.Expt.propprExamplesAsData(db,exampleFile)
         print 'prepared',d.keys()
-#        print 'd.keys()',d.keys()
-#        print 'd.values',map(type,d.values())
-#        scipy.io.savemat(cacheFile,d, do_compression=True)
-#        print 'saved in',cacheFile
         return d
-#    else:
-#        d = {}
-#        scipy.io.loadmat(cacheFile,d)
-#        # bleeping scipy.io utils convert from csr_matrix to csc_matrix
-#        # when you serialize/deserialize, like they think it doesn't
-#        # make a difference or somethin'
-#        for k in d.keys():
-#            if not k.startswith("__"):
-#                print 'k',k,'type',type(d[k])
-#                (X,Y) = d[k]
-#                d[k] = (X.tocsr(),Y.tocsr())
-#                print 'loading matrix',k,mutil.numRows(d[k]),'rows',d[k].nnz,'non-zeros'
-#        return d
         
 if __name__=="__main__":
-#    d = uncacheMatPairs('cora-XY.mat','cora.db','train_samebib','test_samebib')
-    dTrain = uncacheMatPairs('cora-XY.mat','cora.db','raw/train.examples')
-    dTest = uncacheMatPairs('cora-XY.mat','cora.db','raw/test.examples')
+    dTrain = uncacheMatPairs('cora-XY.mat','cora.db','inputs/train.examples')
+    dTest = uncacheMatPairs('cora-XY.mat','cora.db','inputs/test.examples')
     prog = tensorlog.ProPPRProgram.load(["cora.db","cora.ppr"])
     prog.db.markAsParam('kaw',1)
     prog.db.markAsParam('ktw',1)
--- datasets/cora/debug.py
+++
@@ -1,8 +0,0 @@
-import tensorlog
-import declare
-import learn
-import time
-
-if __name__=="__main__":
-    mode = declare.ModeDeclaration('samebib(i,o)')
-    ti = tensorlog.Interp(initFiles=["cora.cfacts","cora.ppr"])
