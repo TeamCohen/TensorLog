6c195650d30bdeb3c6a05b2e54a277acde213d5c
William Cohen <wcohen@cs.cmu.edu>
2016-05-31 17:03:49 -0400

    missing files

---
+++ src/config.py
@@ -0,0 +1,30 @@
+# (C) William W. Cohen and Carnegie Mellon University, 2016
+
+# A class to contain configuration information
+
+class Config(object):
+
+    def __init__(self):
+        self.help = ConfigHelp()
+
+    """A container for configuration options"""
+    def pprint(self,depth=0):
+        if not depth: print '===='
+        for key,val in self.__dict__.items():
+            if key!='help':
+                self._explain(depth,key,val)
+                if type(val)==type(Config()):
+                    val.pprint(depth+1)
+
+    def _explain(self,depth,key,val):
+        tmp = '| '*depth + key + ':'
+        if type(val)!=type(Config()):
+            tmp += ' '+repr(val)
+        if key in self.help.__dict__:
+            print '%-40s %s' % (tmp,self.help.__dict__[key])
+            
+
+class ConfigHelp(object):
+    """A parallel object that stores help about configurations."""
+    pass
+
