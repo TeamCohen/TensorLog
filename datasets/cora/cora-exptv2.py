7799d2866eec848c260cc37a4e278555b9d05aaf
William Cohen <wcohen@furter.ml.cmu.edu>
2016-06-28 16:05:00 -0400

    back to maxdepth 1

--- datasets/cora/cora-exptv2.py
+++ datasets/cora/cora-exptv2.py
@@ -26,7 +26,7 @@ if __name__=="__main__":
     prog.db.markAsParam('kaw',1)
     prog.db.markAsParam('ktw',1)
     prog.db.markAsParam('kvw',1)
-    prog.maxDepth = 2
+    prog.maxDepth = 1
     ops.conf.optimize_component_multiply = True
     params = {'initProgram':prog,
               'trainData':trainData, 'testData':testData,
