1646d79378d9b65f91c52e70cc9b4a0dad41d247
William Cohen <wcohen@furter.ml.cmu.edu>
2016-07-01 13:49:17 -0400

    updated to new apis

--- datasets/wordnet/wnet-expt.py
+++ datasets/wordnet/wnet-expt.py
@@ -10,25 +10,26 @@ import ops
 import learn
 
 if __name__=="__main__":
+    #usage: [targetPredicate] [epochs]
     pred = 'hypernym' if len(sys.argv)<=1 else sys.argv[1]
-    epochs = 10 if len(sys.argv)<=2 else int(sys.argv[2])
-        
-    print '== pred',pred,'epochs',epochs
-    db = matrixdb.MatrixDB.uncache('wnet.db','wnet.cfacts')
-    trainData = dataset.Dataset.uncacheExamples('%s-train.dset' % pred,db,'%s-train.examples' % pred,proppr=True)
-    testData = dataset.Dataset.uncacheExamples('%s-test.dset' % pred,db,'%s-test.examples' % pred,proppr=True)
-    prog = tensorlog.ProPPRProgram.load(["wnet-learned.ppr"],db=db)
-    prog.setWeights(db.vector(declare.asMode("rule(i)")))
-    def learnerFactory(prog):
-        return learn.FixedRateGDLearner(prog,regularizer=learn.L2Regularizer(),traceFun=learn.Learner.cheapTraceFun,epochs=10)
+    epochs = 30 if len(sys.argv)<=2 else int(sys.argv[2])
+    optdict,args = tensorlog.parseCommandLine([
+            '--db', 'wnet.db|wnet.cfacts',
+            '--prog','wnet-learned.ppr', '--proppr',
+            '--train','%s-train.dset|%s-train.examples' % (pred,pred),
+            '--test', '%s-test.dset|%s-test.examples' % (pred,pred)])
+    prog = optdict['prog']
+    prog.setWeights(prog.db.vector(declare.asMode("rule(i)")))
+    learner = learn.FixedRateGDLearner(prog,regularizer=learn.L2Regularizer(),traceFun=learn.Learner.cheapTraceFun,epochs=epochs)
 
     #ops.conf.trace = True
-    params = {'initProgram':prog,
-              'trainData':trainData, 'testData':testData,
-              'targetPred':'i_%s/io' % pred,
-              'savedTestPreds':'%s-test.solutions.txt' % pred,
-              'savedTrainExamples':'%s-train.examples' % pred,
-              'savedTestExamples':'%s-test.examples' % pred,
-              'learnerFactory':learnerFactory,
+    params = {'prog':prog,
+              'trainData':optdict['trainData'], 
+              'testData':optdict['testData'],
+              'targetMode':'i_%s/io' % pred,
+              'savedTestPredictions':'tmp-cache/%s-test.solutions.txt' % pred,
+              'savedTrainExamples':'tmp-cache/%s-train.examples' % pred,
+              'savedTestExamples':'tmp-cache/%s-test.examples' % pred,
+              'learner':learner
     }
     expt.Expt(params).run()
