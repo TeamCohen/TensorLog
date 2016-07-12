229b396d92cca9c7aaff7a8ee6424cd1aca1de85
William Cohen <wcohen@cs.cmu.edu>
2016-07-12 16:48:21 -0400

    oops forgot this

--- datasets/fb15k-237/fb-expt.py
+++ datasets/fb15k-237/fb-expt.py
@@ -8,33 +8,47 @@ import tensorlog
 import learn
 import plearn
 
+SERIAL=False
+
 if __name__=="__main__":
-    #usage: fb-expt.py [epochs] [targetMode] 
+    #usage: fb-expt.py [processes] [epochs] [targetMode] 
     
-    epochs = int(sys.argv[1]) if len(sys.argv)>1 else 10
-    targetMode = sys.argv[2] if len(sys.argv)>2 else None
+    processes = int(sys.argv[1]) if len(sys.argv)>1 else 1
+    epochs = int(sys.argv[2]) if len(sys.argv)>2 else 10
+    targetMode = sys.argv[3] if len(sys.argv)>3 else None
     if targetMode and (not targetMode.endswith("(i,o)")): 
         targetMode += "(i,o)"
 
-    optdict,args = tensorlog.parseCommandLine([
+    optdict,args = expt.Expt.timeAction(
+        'parsing command line and loading data',
+        lambda:tensorlog.parseCommandLine([
             '--logging','info',
             '--db', 'fb.db|fb.cfacts',
             '--prog','learned-rules.ppr', '--proppr',
             '--train','train.dset|train.exam',
             '--test', 'valid.dset|valid.exam'
-        ])
+        ]))
 
     prog = optdict['prog']
     prog.setWeights(prog.db.vector(declare.asMode("ruleid(i)")))
-    learner = learn.FixedRateGDLearner(
-        prog,
-        epochs=epochs,
-#        regularizer=learn.L2Regularizer(),
-#        traceFun=learn.Learner.cheapTraceFun,
+    if processes==0:
+        learner = learn.FixedRateGDLearner(
+            prog,
+            epochs=epochs,
+            #        regularizer=learn.L2Regularizer(),
+            #        traceFun=learn.Learner.cheapTraceFun,
         )
-    #learner = plearn.ModeParallelFixedRateGDLearner(prog,regularizer=learn.L2Regularizer(),traceFun=learn.Learner.cheapTraceFun)
+    else:
+        learner = plearn.ParallelFixedRateGDLearner(
+            prog,
+            epochs=epochs,
+            parallel=processes,
+            miniBatchSize=100,
+            #        regularizer=learn.L2Regularizer(),
+            #        traceFun=learn.Learner.cheapTraceFun,
+        )
 
-    # configute the experiment
+    # configure the experiment
     params = {'prog':prog,
               'trainData':optdict['trainData'], 
               'testData':optdict['testData'],
