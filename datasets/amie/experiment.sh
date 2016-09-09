ddcf971f6bc0fcd95e5b880d14244eaf0cea8c98
Kathryn Mazaitis <krivard@cs.cmu.edu>
2016-09-09 13:56:14 -0400

    added proppr routines to amie experiment

--- datasets/amie/experiment.sh
+++ datasets/amie/experiment.sh
@@ -16,17 +16,22 @@
 #make clean reset
 #IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=1 make -Be phase
 
-make clean reset
-IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=0 make -Be phase
-make clean reset
-IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=1 make -Be phase
-make clean reset
-IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=0 make -Be phase
-make clean reset
-IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=2 make -Be phase
+#make clean reset
+#IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=0 make -Be phase
+#make clean reset
+#IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=1 make -Be phase
+#make clean reset
+#IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=0 make -Be phase
 
-# epochs reduction
-make clean reset
-IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=0 EPOCHS=10 make -Be phase
-make clean reset
-IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=1 EPOCHS=10 make -Be phase
+## failed
+#make clean reset
+#IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=2 make -Be phase
+#make clean reset
+IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=2 PARALLEL=10 make -e phase
+
+
+
+## proppr untrained inference
+#IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=0 make -e proppr
+#IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=0 make -e proppr
+
