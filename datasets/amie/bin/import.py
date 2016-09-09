819e1cf4bedf651174f1808c6fc741b3b1e9c3e6
Kathryn Mazaitis <krivard@cs.cmu.edu>
2016-09-09 09:01:16 -0400

    amie experiment refinement for core KB

--- datasets/amie/bin/import.py
+++ datasets/amie/bin/import.py
@@ -4,22 +4,27 @@ import convertRules
 import convertFacts
 import sys
 
-PREFIX_SINGLE=""
-PREFIX_RECURSIVE="i_"
-PREFIX_MANUAL="r"
+#PREFIX_SINGLE=""
+#PREFIX_RECURSIVE="i_"
+#PREFIX_MANUAL="r"
 
 
 if __name__=="__main__":
+    if len(sys.argv) < 3:
+        print "Usage: %s dataset name [param=value ...]" % sys.argv[0]
+        sys.exit(1)
+    dataset = sys.argv[1]
+    name = sys.argv[2]
     opts = {}
     # defaults:
     opts['infn'] = '/remote/curtis/wcohen/data/amie/rules/amie/amie_yago2_sample_support_2.tsv'
-    opts['outfnstem'] = 'inputs/yago2-sample'
+    opts['outfnstem'] = 'inputs/%s-%s' % (dataset,name)
     opts['recursive'] = False
     
-    if len(sys.argv)>1:
-        for setting in sys.argv[1:]:
+    if len(sys.argv)>3:
+        for setting in sys.argv[3:]:
             kw,arg = setting.split("=")
-            if kw not in opts and kw is not 'manualRecursion':
+            if kw not in opts:
                 print "Syntax error: '{0}' not recognized. ".format(kw)
                 print "Possible options: {0}".format(", ".join(opts.keys()))
                 sys.exit(1)
@@ -34,4 +39,4 @@ if __name__=="__main__":
     #convertRules.convert(src,dst,PREFIX_MANUAL,groundInverses=False,manualRecursion=1)
     
     #convertFacts.convert('/remote/curtis/wcohen/data/amie/kbs/yago2/yago2core.10kseedsSample.compressed.notypes.tsv','inputs/yago2-sample-core.cfacts')
-    convertFacts.convert('/remote/curtis/wcohen/data/amie/kbs/yago2/yago2core.10kseedsSample.compressed.notypes.tsv','{0}-core.cfacts'.format(opts['outfnstem']),includeInverse=True)
+    convertFacts.convert('src/{0}.tsv'.format(dataset),'{0}-core.cfacts'.format(opts['outfnstem']),includeInverse=True)
--- datasets/amie/experiment.sh
+++ datasets/amie/experiment.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
+### yago2-sample
 #make clean reset
-#IMPORT="recursive=False" D=yago2-sample-nonrecursive DEPTH=0 make -Be phase
+#IMPORT="recursive=False" D=yago2-sample N=nonrecursive DEPTH=0 make -Be phase
 #make clean reset
-#IMPORT="recursive=True" D=yago2-sample-recursive DEPTH=0 make -Be phase
+#IMPORT="recursive=True" D=yago2-sample N=recursive DEPTH=0 make -Be phase
 #make clean reset
-#IMPORT="recursive=True" D=yago2-sample-recursive DEPTH=1 make -Be phase
+#IMPORT="recursive=True" D=yago2-sample N=recursive DEPTH=1 make -Be phase
 #make clean reset
-#IMPORT="recursive=True" D=yago2-sample-recursive DEPTH=2 make -Be phase
+#IMPORT="recursive=True" D=yago2-sample N=recursive DEPTH=2 make -Be phase
+#make clean reset
+#IMPORT="recursive=True" D=yago2-sample N=recursive DEPTH=3 PARALLEL=10 make -Be phase
+
+### yago2-core
+#make clean reset
+#IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=1 make -Be phase
+
 make clean reset
-IMPORT="recursive=True" D=yago2-sample-recursive DEPTH=3 PARALLEL=10 make -Be phase
+IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=0 make -Be phase
+make clean reset
+IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=1 make -Be phase
+make clean reset
+IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=0 make -Be phase
+make clean reset
+IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=2 make -Be phase
+
+# epochs reduction
+make clean reset
+IMPORT="recursive=False" D=yago2-core N=nonrecursive DEPTH=0 EPOCHS=10 make -Be phase
+make clean reset
+IMPORT="recursive=True" D=yago2-core N=recursive DEPTH=1 EPOCHS=10 make -Be phase
