6075b555dfd831f9272f2f285afc914f3ccb72a6
William Cohen <wcohen@gmail.com>
2017-04-20 15:07:44 -0400

    added tensorflow-based experiment

--- datasets/grid/testexpt.py
+++ datasets/grid/testexpt.py
@@ -1,10 +1,11 @@
 import unittest
+import tfexpt
 import expt
 
 from tensorlog import matrixdb
 from tensorlog import program
 
-class TestAcc(unittest.TestCase):
+class TestAccNative(unittest.TestCase):
 
   def setUp(self):
     (self.n,self.maxD,self.epochs) = (16,8,20)
@@ -18,6 +19,17 @@ class TestAcc(unittest.TestCase):
     times = expt.timingExpt(self.prog)
     for t in times:
       self.assertTrue(t < 0.005)
+
+class TestAccTF(unittest.TestCase):
+
+  def setUp(self):
+    (self.n,self.maxD,self.epochs) = (16,8,20)
+    (self.factFile,self.trainFile,self.testFile) = expt.genInputs(self.n)
+    (self.tlog,self.trainData,self.testData) = tfexpt.setup_tlog(self.maxD,self.factFile,self.trainFile,self.testFile)
+
+  def testIt(self):
+    acc = tfexpt.trainAndTest(self.tlog,self.trainData,self.testData,self.epochs)
+    self.assertTrue(acc >= 0.95)
 
 if __name__ == "__main__":
   unittest.main()
---
+++ datasets/grid/tfexpt.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+
+from tensorlog import simple
+import expt
+
+def setup_tlog(maxD,factFile,trainFile,testFile):
+  tlog = simple.Compiler(db=factFile,prog="grid.ppr")
+  tlog.prog.db.markAsParam('edge',2)
+  tlog.prog.maxDepth = maxD
+  trainData = tlog.load_small_dataset(trainFile)
+  testData = tlog.load_small_dataset(testFile)
+  return (tlog,trainData,testData)
+
+def trainAndTest(tlog,trainData,testData,epochs):
+  mode = 'path/io'
+  predicted_y = tlog.inference(mode)
+  actual_y = tlog.target_output_placeholder(mode)
+  correct_predictions = tf.equal(tf.argmax(actual_y,1), tf.argmax(predicted_y,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+
+  unregularized_loss = tlog.loss(mode)
+  optimizer = tf.train.AdagradOptimizer(1.0)
+  train_step = optimizer.minimize(unregularized_loss)
+
+  session = tf.Session()
+  session.run(tf.global_variables_initializer())
+  (tx,ty) = trainData[mode]
+  train_fd = {tlog.input_placeholder_name(mode):tx, tlog.target_output_placeholder_name(mode):ty}
+  for i in range(epochs):
+    print 'epoch',i+1
+    session.run(train_step, feed_dict=train_fd)
+  (ux,uy) = testData[mode]
+  test_fd = {tlog.input_placeholder_name(mode):ux, tlog.target_output_placeholder_name(mode):uy}
+  acc = session.run(accuracy, feed_dict=test_fd)
+  print 'test acc',acc
+  return acc
+
+
+def runMain():
+  (goal,n,maxD,epochs) = expt.getargs()
+  assert goal=="acc"
+  (factFile,trainFile,testFile) = expt.genInputs(n)
+  (tlog,trainData,testData) = setup_tlog(maxD,factFile,trainFile,testFile)
+  trainAndTest(tlog,trainData,testData,epochs)
+
+
+if __name__=="__main__":
+  runMain()
