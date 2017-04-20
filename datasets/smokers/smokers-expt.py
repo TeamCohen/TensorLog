93e26a620a46e9d73d26a422190b329fe558f114
Kathryn Mazaitis <krivard@cs.cmu.edu>
2017-04-20 10:44:32 -0400

    added xcomp to smokers dataset

--- datasets/smokers/smokers-expt.py
+++ datasets/smokers/smokers-expt.py
@@ -6,23 +6,56 @@ from tensorlog import program
 from tensorlog import expt
 from tensorlog import declare
 from tensorlog import mutil
+from tensorlog import xctargets
+
+CROSSCOMPILERS = []
+if xctargets.theano:
+  from tensorlog import theanoxcomp
+  for c in [
+    theanoxcomp.DenseMatDenseMsgCrossCompiler,
+    theanoxcomp.SparseMatDenseMsgCrossCompiler
+    ]:
+    CROSSCOMPILERS.append(c)
+if xctargets.tf:
+  from tensorlog import tensorflowxcomp
+  for c in [
+    tensorflowxcomp.DenseMatDenseMsgCrossCompiler,
+    tensorflowxcomp.SparseMatDenseMsgCrossCompiler,
+    ]:
+    CROSSCOMPILERS.append(c)
 
 if __name__=="__main__":
 
     optdict,args = comline.parseCommandLine('--prog smokers.ppr --proppr --db smokers.cfacts'.split())
-    ti = program.Interp(optdict['prog'])
-    ti.prog.setRuleWeights()
-    ti.prog.maxDepth = 99
+    prog = optdict['prog']
+    prog.setRuleWeights()
+    prog.maxDepth = 99
     rows = []
     for line in open('query-entities.txt'):
         sym = line.strip()
-        rows.append(ti.db.onehot(sym))
+        rows.append(prog.db.onehot(sym))
     X = mutil.stack(rows)
 
     start0 = time.time()
-    for modeString in ["t_stress/io", "t_influences/io","t_cancer_spont/io", "t_cancer_smoke/io"]:
+    modes = ["t_stress/io", "t_influences/io","t_cancer_spont/io", "t_cancer_smoke/io"]
+    for modeString in modes:
         print 'eval',modeString,
         start = time.time()
-        ti.prog.eval(declare.asMode(modeString), [X])
+        prog.eval(declare.asMode(modeString), [X])
         print 'time',time.time() - start,'sec'
     print 'total time', time.time() - start0,'sec'
+
+    
+    for compilerClass in CROSSCOMPILERS:
+        print compilerClass.__name__
+        start0=time.time()
+        xc = compilerClass(prog)
+        # compile everything
+        for modeString in modes:
+            mode = declare.asMode(modeString)
+            xc.ensureCompiled(mode)
+            print 'eval',modeString,compilerClass.__name__
+            start = time.time()
+            xc.inferenceFunction(mode)(X)
+            print 'time',time.time() - start,'sec'
+        print 'total time',compilerClass.__name__,time.time()-start0,'sec'
