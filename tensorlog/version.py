0f93b8ece742a56c31472f9a98df12986582e2df
William Cohen <wcohen@gmail.com>
2017-06-16 11:17:08 -0400

    docs

--- tensorlog/version.py
+++ tensorlog/version.py
@@ -2,7 +2,7 @@
 #
 # version number tracking for Tensorlog
 
-VERSION = '1.3.4'
+VERSION = '1.3.5'
 
 # externally visible changes:
 #
@@ -51,8 +51,11 @@ VERSION = '1.3.4'
 #       db.serializeDataTo(filelike,filter=None|params|noparams)
 #       db.importSerializeDataFrom(filelike)
 #     pythonic syntax for parser (automatic for files with extension .tlog)
-# version 1.3.4:
+# version 1.3.5:
 #     api typo fix: db.importSerializeDataFrom(filelike) -> importSerializedDataFrom(filelike)
 #     simple.compiler.load_xxx_examples can be passed file-like objects
 #     builder rules will print in pythonic syntax
+#     now working
+#        builder.db = "dbspec"
+#        builder.db += "file"
 #     bug fixes
