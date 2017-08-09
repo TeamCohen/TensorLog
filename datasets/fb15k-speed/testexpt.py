10e55395bb6600693e780574142ea37369a6874a
William Cohen <wcohen@arnout.ml.cmu.edu>
2017-08-09 11:15:00 -0400

    fixed timing numbers for cmu gpus

--- datasets/fb15k-speed/testexpt.py
+++ datasets/fb15k-speed/testexpt.py
@@ -4,16 +4,19 @@ import expt
 
 class TestTimeNative(unittest.TestCase):
 
+  # these tests are GPU-dependent
   def testIt(self):
     fps,qps1,qps2 = expt.runMain()
-    self.assertTrue(fps >= 1500.0)  # compilation
-    self.assertTrue(qps1 >= 300.0)  # minibatches size = 1
-    self.assertTrue(qps2 >= 2000.0)  # minibatches size = as large as possible
+    print 'fps,qps1,qps2 are',fps,qps1,qps2
+    self.assertTrue(fps >= 700.0)  # compilation
+    self.assertTrue(qps1 >= 250.0)  # minibatches size = 1
+    self.assertTrue(qps2 >= 1500.0)  # minibatches size = as large as possible
 
 class TestTimeTF(unittest.TestCase):
 
   def testIt(self):
     fps,qps = tfexpt.runMain()
+    print 'fps,qps are',fps,qps
     self.assertTrue(fps >= 2.0)
     self.assertTrue(qps >= 20.0)
 
