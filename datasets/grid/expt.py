41e47e71c8ed678d4601c8c1901e5ce78f0924da
Kathryn Mazaitis <krivard@cs.cmu.edu>
2017-08-24 14:30:45 -0400

    grid dataset: removed tf from native expt, added dataset loading to unittest

--- datasets/grid/expt.py
+++ datasets/grid/expt.py
@@ -36,14 +36,6 @@ if xctargets.theano:
     ]:
     CROSSCOMPILERS.append(c)
     CROSSLEARNERS[c]=theanoxcomp.FixedRateGDLearner
-if xctargets.tf:
-  from tensorlog import tensorflowxcomp
-  for c in [
-    tensorflowxcomp.DenseMatDenseMsgCrossCompiler,
-    tensorflowxcomp.SparseMatDenseMsgCrossCompiler,
-    ]:
-    CROSSCOMPILERS.append(c)
-    CROSSLEARNERS[c]=tensorflowxcomp.FixedRateGDLearner
 
 VISUALIZE = False
 
--- datasets/grid/testexpt.py
+++ datasets/grid/testexpt.py
@@ -4,18 +4,22 @@ import expt
 
 from tensorlog import matrixdb
 from tensorlog import program
+from tensorlog import dataset
 
 class TestNative(unittest.TestCase):
 
   def setUp(self):
     (self.n,self.maxD,self.epochs) = (16,8,20)
-    (self.factFile,self.trainFile,self.testFile) = expt.genInputs(self.n)
+    (self.factFile,trainFile,testFile) = expt.genInputs(self.n)
+
 #    (self.factFile,self.trainFile,self.testFile) = ('inputs/g16.cfacts','inputs/g16-train.exam','inputs/g16-test.exam')
     self.db = matrixdb.MatrixDB.loadFile(self.factFile)
     self.prog = program.Program.loadRules("grid.ppr",self.db)
+    self.trainData = dataset.Dataset.loadExamples(self.prog.db,trainFile)
+    self.testData = dataset.Dataset.loadExamples(self.prog.db,testFile)
 
   def testIt(self):
-    acc,loss = expt.accExpt(self.prog,self.trainFile,self.testFile,self.n,self.maxD,self.epochs)
+    acc,loss = expt.accExpt(self.prog,self.trainData,self.testData,self.n,self.maxD,self.epochs)
     print 'acc',acc
     self.assertTrue(acc >= 0.85)
     times = expt.timingExpt(self.prog)
