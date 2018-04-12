650161ce4be9abf6c802e695e772b7d0fe2fd642
William Cohen <wcohen@furter.ml.cmu.edu>
2018-04-12 17:49:35 -0400

    compactsketch

--- tensorlog/helper/countmin_embeddings.py
+++ tensorlog/helper/countmin_embeddings.py
@@ -6,11 +6,12 @@ import math
 import random
 import sys
 import getopt
+import collections
 
-from tensorlog import matrixdb,comline
+from tensorlog import matrixdb,comline,mutil
 
 LOGBASE = 2
-MAX_NUM_HASHES = 100
+MAX_NUM_HASHES = 1000
 
 def componentwise_min(X,Y):
   """ componentwise_min for two scipy matrices
@@ -50,7 +51,6 @@ class Sketcher(object):
     self.hash_salt = [ random.getrandbits(32) for _ in range(MAX_NUM_HASHES) ]
     self.offset = hash("389437892342389")
     self.init_hashmats()
-    print 'n',self.n,'t',self.t,'m',self.m,'sketch size',self.t*self.m,'compression',float(self.n)/(self.t*self.m)
 
   def init_hashmats(self):
     """ self.hashmat caches out sketch for each object 0...n.  So if x is
@@ -128,7 +128,7 @@ class Sketcher(object):
     X.dot(hashmat)=S, return the sketch for X.dot(M).
     """
     M = self.db.matEncoding[(rel,2)]
-    return self.unsketch(S).dot(M).dot(self.hashmat)
+    return self.denoise(S.dot(M))
 
   def showme(self,**kw): 
     """ For debugging - print a summary of some objects, especially
@@ -147,9 +147,14 @@ class Sketcher(object):
             print self.db.rowAsSymbolDict(val),        
       except AttributeError:
         print tag,'=',val
+      except ValueError:
+        print tag,'=',val
       print
 
   def compareRows(self,gold,approx):
+    """ For debugging, compare gold and approximate vectors to show how
+    different they are
+    """
     dGold = self.db.rowAsSymbolDict(gold)
     dApprox = self.db.rowAsSymbolDict(approx)
     fp = set(dApprox.keys()) - set(dGold.keys())
@@ -157,6 +162,72 @@ class Sketcher(object):
     print 'errors:',len(fp),list(fp)[0:10]
     if len(fn)>0:
       print '  also there are false negatives - really?',fn
+
+class CompactSketcher(Sketcher):
+  """
+  Slightly different plan - to follow a sketch S thru a relation
+  matrix M, look at the coo_matrix version of M, which is basically
+  k_M triples of the form (i,j,w) where i is a row index, j is a
+  column index, and w is a weight.  For each M, we construct a
+  something like hashmats above, but instead of Hd mapping an entity
+  index to [1...m], so Hd[e]=hash(d,e), we let Hd maps an index k from
+  [1...k_m], via the row entity i, into [1...m].  Then Hd^T maps S to
+  the triples from the coo_matrix for M.  We can 'follow' M by using
+  the componentwise_min of the S.Hd^T's to find the non-zero triple
+  indices, multiplying by the w's, and then finally using another hash
+  to map the j's into sketch space.
+  
+  This Hd is stored in sketchmatsArg1[rel][d], where rel is the
+  functor associated with M.  We also have a second analogous hash
+  that maps the j's to sketch space, called sketchmatsArg2[rel].  We
+  don't both computing and storing sketchmatArg1[rel] or
+  sketchmatsArg2[rel][d], since they are never used.
+  """
+
+  def __init__(self,db,k,delta):
+    # want to make t 2b * as large
+    super(CompactSketcher,self).__init__(db,k,delta)
+    # these are like hashmat, and hashmats, but indexed by the functor
+    # for a relation
+    self.sketchmatsArg1 = collections.defaultdict(list)
+    self.sketchmatArg2 = {}
+    self.sketchmatWeights = {}
+    print 'sketching the db rels....'
+    for (functor,arity),M in db.matEncoding.items():
+      if arity==2:
+        m = scipy.sparse.coo_matrix(M)
+        self.sketchmatWeights[functor] = scipy.sparse.csr_matrix(m.data.reshape((1,m.nnz)))
+        skShape = (m.nnz,self.m*self.t)  #shape of the sketches we build below
+        skRows = np.arange(m.nnz)        #coo.row for sketches we build
+        skArg2 = scipy.sparse.coo_matrix(skShape)
+        ones = np.ones_like(skRows)
+        for d in range(self.t):
+          arrayHasher = np.vectorize(lambda k: self.hash_function(d,k))
+          h1d = scipy.sparse.coo_matrix((ones,(skRows,arrayHasher(m.row))),shape=skShape)
+          self.sketchmatsArg1[functor].append(scipy.sparse.csr_matrix(h1d))
+          h2d = scipy.sparse.coo_matrix((ones,(skRows,arrayHasher(m.col))),shape=skShape)
+          skArg2 = skArg2 + h2d
+        self.sketchmatArg2[rel] = scipy.sparse.csr_matrix(skArg2)
+    print 'done'
+
+  def describe(self):
+    print 'n',self.n,'k',self.k,'delta',self.delta,
+    print 'm',self.m,'sketch size',self.t*self.m,'compression',float(self.n)/(self.t*self.m)
+
+  def follow(self,rel,S):
+    """ The analog of an operator X.dot(M) in sketch space.  Given S where
+    X.dot(hashmat)=S, return the sketch for X.dot(M).
+    """
+    # nz_indices will be the indices of the coo_matrix for rel where
+    # the row# hashes to something in S - unsketch these indices the
+    # way we did before
+    nz_indices = scipy.sparse.csr_matrix(S.dot(self.sketchmatsArg1[rel][0].transpose()))
+    for d in range(1,self.t):
+      in_d = scipy.sparse.csr_matrix(S.dot(self.sketchmatsArg1[rel][d].transpose()))
+      nz_indices = componentwise_min(nz_indices,in_d)
+    # multiply these indices by the corresponding weights, and then
+    # move back to sketch space
+    return self.sketchmatWeights[rel].multiply(nz_indices).dot(self.sketchmatArg2[rel])
 
 # example: python countmin_embeddings.py --db  'fb15k.db|../../datasets/fb15k-speed/inputs/fb15k-valid.cfacts'  --x m_x_02md_2 --rel american_football_x_football_coach_position_x_coaches_holding_this_position_x_american_football_x_football_historical_coach_position_x_team --k 50
 # example: python countmin_embeddings.py --db  'g64.db|../../datasets/grid/inputs/g64.cfacts'  --x 25,36 --rel edge --k 10
@@ -174,9 +245,9 @@ if __name__ == "__main__":
   delta = float(optdict.get('--delta','0.01'))
 
   M_edge = db.matEncoding[(rel,2)]
-  M_edge.data[M_edge.data>0] = 1.0
 
-  sk = Sketcher(db,k,delta/db.dim())
+  sk = CompactSketcher(db,k,delta/db.dim())
+  sk.describe()
 
   def probe(x):
     # find a sketch for x and then invert it
@@ -188,6 +259,7 @@ if __name__ == "__main__":
 
   x = db.onehot(xsym)
   probe(x)
+
   probe(x.dot(M_edge))
 
   print '-'*60
@@ -197,4 +269,23 @@ if __name__ == "__main__":
   nx = x.dot(M_edge)
   sk.showme(nx=nx, sk_nx=sk_nx, approx_nx=approx_nx)
   sk.compareRows(nx,approx_nx)
+
+  # m=3, t=2
+  H = np.array([[0,1,0,1,0,0],[0, 1, 0, 0, 1, 0]])
+  a = np.array([1,0])
+  b = np.array([0,1])
+  # this seems to work 
+  #M = np.array([[0,1],[1,0]])
+  # not this
+  M = np.array([[0,1],[1,1]])
+  sM = 0.5*H.transpose().dot(M).dot(H)
+  sa = a.dot(H)
+  sb = b.dot(H)
+  def clean(M): 
+    M1 = M+0
+    M1[M1>=1] = 1.0
+    M1[M1<1] = 0.0
+    return M1
+  fa = sa.dot(sM)
+  fb = sb.dot(sM)
 
