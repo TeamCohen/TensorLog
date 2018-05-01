94917f3319c73656475a845c35ee8c5693c8cdb0
Kathryn Mazaitis <krivard@cs.cmu.edu>
2018-05-01 09:12:23 -0400

    moved sketch adapters to module code

--- datasets/family/sketch-learn.py
+++ datasets/family/sketch-learn.py
@@ -4,7 +4,8 @@ import getopt
 import scipy.sparse as SS
 import scipy.io
 import numpy as np
-from tensorlog import expt,dataset,comline,matrixdb,mutil,program,funs,sketchcompiler,learn,declare
+from tensorlog import expt,dataset,comline,matrixdb,mutil,program,funs,sketchcompiler,declare
+from tensorlog.helper.sketchadapters import SketchData,SketchLearner
 from tensorlog.helper.fast_sketch import FastSketcher2# as DefaultSketcher
 from tensorlog.helper.fast_sketch import FastSketcher# as DefaultSketcher
 from tensorlog.helper.countmin_embeddings import Sketcher# as DefaultSketcher
@@ -19,18 +20,6 @@ SKETCHERS={
     "Sketcher":Sketcher,
     "Sketcher2":Sketcher2
     }
-
-def sketch_dataset(dset,sk):
-    xDict = {}
-    yDict = {}
-    for mode in dset.modesToLearn():
-        try:
-            xDict[mode] = sk.sketch(dset.getX(mode))
-            yDict[mode] = sk.sketch(dset.getY(mode))
-        except:
-            print mode
-            raise
-    return dataset.Dataset(xDict,yDict)
 
 stem = "kinship"
 def setExptParams():
@@ -66,14 +55,13 @@ def setExptParams():
     skTest = SketchData(testData,sketcher,"test")
     
     prog = program.ProPPRProgram.loadRules("theory.ppr",db=db)
-    prog.compilerDef = lambda m,p,d,r: sketchcompiler.SketchCompiler(m,p,d,r,sketcher)
-    prog.softmaxDef = lambda f:sketchcompiler.SketchSoftmaxFunction(f,sketcher)
+    sketchProgram(prog,sketcher)
     prog.setRuleWeights()
     prog.maxDepth=4
     return (prog, skTrain,skTest, sketcher, (k,delta,sketcher_s))
 
-mode = declare.asMode("i_niece/io")
-probe = False
+mode = declare.asMode("i_brother/io")
+probe = True
 
 def runMain():
     if not os.path.exists("tmp-cache"): os.mkdir("tmp-cache")
@@ -84,12 +72,11 @@ def runMain():
         return (None, prog, testData, mode, sk)
     else:
         insert_sk_funs(prog.getFunction(mode),sk)
-        learner = SketchLearner(sk,prog,epochs=4)
-        learner.listen.append(trainData)
-        learner.listen.append(testData)
-        p.probe(learner,prog,testData,mode)
+        learner = SketchLearner(sk,[trainData,testData],prog,epochs=4)
+        p.probe(learner,prog,trainData,mode)
         return (learner,prog,testData,mode,sk)
 
+# this is only needed for generating sensical trace output
 def insert_sk_funs(fun,sk):
     if hasattr(fun,'sk'):return
     fun.sk = sk
@@ -98,9 +85,7 @@ def insert_sk_funs(fun,sk):
         for f in fun.funs: insert_sk_funs(f,sk)
     
 def accExpt(prog,trainData,testData,sketcher,args):
-    learner = SketchLearner(sketcher,prog,epochs=10)
-    learner.listen.append(trainData)
-    learner.listen.append(testData)
+    learner = SketchLearner(sketcher,[trainData,testData],prog,epochs=10)
     params = {'prog':prog,
               'trainData':trainData,
               'testData':testData,
@@ -111,77 +96,6 @@ def accExpt(prog,trainData,testData,sketcher,args):
               'learner':learner,
     }
     return expt.Expt(params).run()
-
-class SketchData(dataset.Dataset):
-    def __init__(self,image,sketcher,name):
-        self.native = image
-        self.name=name
-        self.sketch = sketch_dataset(self.native,sketcher)
-        self.toggle('XY','sketch')
-    def toggle(self,vars,version):
-        for var in vars:
-            if var == 'Y':
-                if version == 'native':
-                    self.yDict = self.native.yDict
-                elif version == 'sketch':
-                    self.yDict = self.sketch.yDict
-                else:
-                    assert False, "Bad version %s" % version
-            elif var == 'X':
-                if version == 'native':
-                    self.xDict = self.native.xDict
-                elif version == 'sketch':
-                    self.xDict = self.sketch.xDict
-                else:
-                    assert False, "Bad version %s" % version
-            else:
-                assert False, "Bad var %s" % var
-
-class SketchLearner(learn.FixedRateGDLearner):
-    def __init__(self,sketcher,prog,**kwargs):
-        super(SketchLearner,self).__init__(prog,**kwargs)
-        self.sketcher = sketcher
-        self.listen=[]
-        self.predict = self.native_predict
-        #self.state = "pretrain"
-    def native_predict(self,mode,X,pad=None):
-        P=super(SketchLearner,self).predict(mode,X,pad)
-        UP=self.sketcher.unsketch(P)
-        #print mode,mutil.pprintSummary(P),mutil.pprintSummary(UP)
-        return UP
-    def sketch_predict(self,mode,X,pad=None):
-        return super(SketchLearner,self).predict(mode,X,pad)
-    def train(self,dset):
-        self.predict=self.sketch_predict
-        for a in self.listen: a.toggle('XY','sketch')
-        super(SketchLearner,self).train(dset)
-    def datasetPredict(self,dset,copyXs=True):
-        self.predict=self.native_predict
-        for a in self.listen:
-            a.toggle('X','sketch')
-            a.toggle('Y','native')
-        result = super(SketchLearner,self).datasetPredict(dset,copyXs)
-        for a in self.listen:
-            a.toggle('X','native')
-        result.xDict = dset.xDict
-        return result
-    def applyUpdate(self,paramGrads,rate):
-        """Add each gradient to the appropriate param, after scaling by rate,
-        and clip negative parameters to zero.
-        """ 
-        paramGrads.fitParameterShapes()
-        for (functor,arity),delta in paramGrads.items():
-            m0 = self.prog.db.getParameter(functor,arity)
-            try:
-              m1 = m0 + rate * self.sketcher.unsketch(delta)
-            except ValueError:
-              print mutil.pprintSummary(m0)
-              print rate
-              print mutil.pprintSummary(delta)
-              raise
-            m2 = mutil.mapData(lambda d:np.clip(d,0.0,np.finfo('float32').max), m1)
-            self.prog.db.setParameter(functor,arity,m2)
-
 
 if __name__=="__main__":
     data = runMain()
---
+++ tensorlog/helper/sketchadapters.py
@@ -0,0 +1,118 @@
+
+from tensorlog import dataset,learn,mutil,sketchcompiler
+import numpy as np
+import logging
+
+"""
+Sample usage:
+
+from tensorlog.helper.sketchadapters import sketchProgram,SketchData,SketchLearner
+
+sketcher = [some Sketcher() init]
+prog = [some Program() init]
+trainData_native = [some Dataset() init]
+testData_native = [some Dataset() init]
+
+sketchProgram(sketcher, prog)
+trainData = SketchData(sketcher, trainData_native)
+testData  = SketchData(sketcher, testData_native)
+learner   = SketchLearner(sketcher, publish=[trainData,testData], prog, ...)
+
+params = {'prog':prog,
+          'trainData':trainData,
+          'testData':testData,
+          'learner':learner
+}
+expt.Expt(params).run()
+"""
+
+def sketchProgram(sk, prog):
+  """Configure a tensorlog Program object to use Sketch-based compile and softmax machinery"""
+  prog.compilerDef = lambda m,p,d,r: sketchcompiler.SketchCompiler(m,p,d,r,sk)
+  prog.softmaxDef = lambda f:sketchcompiler.SketchSoftmaxFunction(f,sk)
+
+def sketchDataset(sk, dset):
+  """Convert an entity-space Dataset object to a SketchDataset (compatible with SketchLearner)"""
+  xDict = {}
+  yDict = {}
+  for mode in dset.modesToLearn():
+      try:
+          xDict[mode] = sk.sketch(dset.getX(mode))
+          yDict[mode] = sk.sketch(dset.getY(mode))
+      except:
+          print mode
+          raise
+  return dataset.Dataset(xDict,yDict)
+  
+class SketchData(dataset.Dataset):
+    def __init__(self,sketcher,image,name="unnamed"):
+        self.sketch = sketchDataset(sketcher,image)
+        self.native = image
+        self.name=name
+        self.toggle('XY','sketch')
+    def toggle(self,vars,version):
+        for var in vars:
+            if var == 'Y':
+                if version == 'native':
+                    self.yDict = self.native.yDict
+                elif version == 'sketch':
+                    self.yDict = self.sketch.yDict
+                else:
+                    assert False, "Bad version %s" % version
+            elif var == 'X':
+                if version == 'native':
+                    self.xDict = self.native.xDict
+                elif version == 'sketch':
+                    self.xDict = self.sketch.xDict
+                else:
+                    assert False, "Bad version %s" % version
+            else:
+                assert False, "Bad var %s" % var
+
+class SketchLearner(learn.FixedRateGDLearner):
+    def __init__(self,sketcher,publish,prog,**kwargs):
+        super(SketchLearner,self).__init__(prog,**kwargs)
+        self.sketcher = sketcher
+        self.publish = publish
+        self.predict = self.native_predict
+    def native_predict(self,mode,X,pad=None):
+        """Return a prediction vector in entity space"""
+        P=super(SketchLearner,self).predict(mode,X,pad)
+        UP=self.sketcher.unsketch(P)
+        #print mode,mutil.pprintSummary(P),mutil.pprintSummary(UP)
+        return UP
+    def sketch_predict(self,mode,X,pad=None):
+        """Return a prediction vector in sketch space"""
+        return super(SketchLearner,self).predict(mode,X,pad)
+    def train(self,dset):
+        self.predict=self.sketch_predict
+        for a in self.publish: a.toggle('XY','sketch')
+        super(SketchLearner,self).train(dset)
+    def datasetPredict(self,dset,copyXs=True):
+        assert dset in self.publish, "predicting on a dataset not subscribed to us is not allowed; it screws up the sketched/unsketched assumptions" 
+        self.predict=self.native_predict
+        for a in self.publish:
+            a.toggle('X','sketch')
+            a.toggle('Y','native')
+        result = super(SketchLearner,self).datasetPredict(dset,copyXs)
+        for a in self.publish:
+            a.toggle('X','native')
+        result.xDict = dset.xDict
+        return result
+    def applyUpdate(self,paramGrads,rate):
+        """Add each gradient to the appropriate param, after scaling by rate,
+        and clip negative parameters to zero.
+        """ 
+        paramGrads.fitParameterShapes()
+        for (functor,arity),delta in paramGrads.items():
+            m0 = self.prog.db.getParameter(functor,arity)
+            try:
+              # delta is in sketch space; the database is in entity space
+              m1 = m0 + rate * self.sketcher.unsketch(delta)
+            except ValueError:
+              print mutil.pprintSummary(m0)
+              print rate
+              print mutil.pprintSummary(delta)
+              raise
+            m2 = mutil.mapData(lambda d:np.clip(d,0.0,np.finfo('float32').max), m1)
+            self.prog.db.setParameter(functor,arity,m2)
